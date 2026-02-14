2174dc5cf281c548783ce14638409b4bc66e3811
ART <popugajarcik@gmail.com>
2026-02-14 14:40:19 +0300

    Note model changes (Name -> Title)

--- models/note.model.go
+++ models/note.model.go
@@ -2,7 +2,7 @@ package models
 
 type Note struct {
 	ID      int    `gorm:"primary_key" json:"id"`
-	Name    string `gorm:"not null" json:"name" validate:"required,min=1,max=100"`
+	Title   string `gorm:"not null" json:"title" validate:"required,min=1,max=100"`
 	Content string `json:"content" validate:"max=5000"`
 
 	UserID int `gorm:"not null" json:"user_id" validate:"required"`
