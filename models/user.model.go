43ed40a1ec741998ee68817c96e3d8dd11760e16
ART <popugajarcik@gmail.com>
2026-02-16 23:22:21 +0300

    user.model update: no default color theme

--- models/user.model.go
+++ models/user.model.go
@@ -6,7 +6,7 @@ type User struct {
 	ID         int64              `gorm:"primaryKey;not null;autoIncrement" json:"id"`
 	Login      string             `gorm:"not null;unique" json:"login" validate:"required,min=1,max=32"`
 	Password   string             `gorm:"not null" json:"password,omitempty" validate:"required,min=8"`
-	ColorTheme user_v1.ColorTheme `gorm:"default:AUTO" json:"color_theme"`
+	ColorTheme user_v1.ColorTheme `json:"color_theme"`
 
 	Notes []Note `gorm:"foreignkey:UserID" json:"notes,omitempty"`
 }
