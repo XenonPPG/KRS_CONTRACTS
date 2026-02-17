1cee1999a1d08aa86cdd3cdf485fef04f19f9845
ART <popugajarcik@gmail.com>
2026-02-17 21:21:36 +0300

    User model last fix

--- models/user.model.go
+++ models/user.model.go
@@ -3,10 +3,10 @@ package models
 import "github.com/XenonPPG/KRS_CONTRACTS/gen/user_v1"
 
 type User struct {
-	ID         int64               `gorm:"primaryKey;not null;autoIncrement" json:"id"`
-	Login      string              `gorm:"not null;unique" json:"login" validate:"required,min=6,max=32"`
-	Password   string              `gorm:"not null" json:"password,omitempty" validate:"required,min=8"`
-	ColorTheme *user_v1.ColorTheme `gorm:"type:integer;default:0" json:"color_theme"`
+	ID         int64              `gorm:"primaryKey;not null;autoIncrement" json:"id"`
+	Login      string             `gorm:"not null;unique" json:"login" validate:"required,min=6,max=32"`
+	Password   string             `gorm:"not null" json:"password,omitempty" validate:"required,min=8"`
+	ColorTheme user_v1.ColorTheme `gorm:"type:integer;default:0" json:"color_theme"`
 
 	Notes []Note `gorm:"foreignkey:UserID" json:"notes,omitempty"`
 }
