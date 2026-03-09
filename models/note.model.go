ae0759dceb18aded46edb863d92cb04dfe762daa
ART <popugajarcik@gmail.com>
2026-03-09 14:06:45 +0300

    added CreatedAt to Note model

--- models/note.model.go
+++ models/note.model.go
@@ -1,9 +1,12 @@
 package models
 
+import "time"
+
 type Note struct {
-	ID      int64  `gorm:"primary_key" json:"id"`
-	Title   string `gorm:"not null" json:"title" validate:"required,min=1,max=100"`
-	Content string `json:"content" validate:"max=5000"`
+	ID        int64     `gorm:"primary_key" json:"id"`
+	Title     string    `gorm:"not null" json:"title" validate:"required,min=1,max=100"`
+	Content   string    `json:"content" validate:"max=5000"`
+	CreatedAt time.Time `json:"created_at"`
 
 	UserID int64 `gorm:"not null" json:"user_id" validate:"required"`
 }
